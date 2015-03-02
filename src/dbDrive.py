d3abdf18a3af01cacd1ff2a3fbff0bfb3d2e77ed
boyangwang <wangboyangjinghua@gmail.com>
2015-03-02 13:11:18 +0800

    dbdriver done

--- src/dbDrive.py
+++ src/dbDrive.py
@@ -5,8 +5,8 @@ class DBDriver(object):
 
 	def __init__(self, host='localhost', port=27017):
 		self.client = pymongo.MongoClient(host, port)
-		self.db = client.ziwei
-		self.collection = db.zhycw
+		self.db = self.client.ziwei
+		self.collection = self.db.zhycw
 
 	def insert(self, doc):
 		docId = self.collection.insert(doc)
