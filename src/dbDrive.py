28de081a67b25508773a07dd20cb3d2c9af46124
boyangwang <wangboyangjinghua@gmail.com>
2015-03-02 13:10:45 +0800

    dbdriver done

--- src/dbDrive.py
+++ src/dbDrive.py
@@ -3,7 +3,7 @@ import pymongo
 
 class DBDriver(object):
 
-	def __init__(self, host='localhost', port='27017'):
+	def __init__(self, host='localhost', port=27017):
 		self.client = pymongo.MongoClient(host, port)
 		self.db = client.ziwei
 		self.collection = db.zhycw
