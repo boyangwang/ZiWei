0eaa6a61f301d06ab1c496afe66c5f88aee642f0
boyangwang <wangboyangjinghua@gmail.com>
2015-05-11 00:06:30 +0800

    print all lines

--- src/crawler.py
+++ src/crawler.py
@@ -319,7 +319,7 @@ def main():
 	#for inputs in inputsArray:
 	#	createPanObjectFromInputs(inputs)
 
-	panObj = createPanObjectFromInputs(inputs, http=True, offline=True)
+	panObj = createPanObjectFromInputs(inputs, http=True, offline=False)
 	# print panObj
 	print 'Elapsed time: ' + str(time.clock())
 
